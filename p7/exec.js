7e36605c5626780e42608dd32dfc104ceb2abb8b
marcos vinicius <marcosviniciussilvacolares@gmail.com>
2023-08-21 08:33:39 -0300

    .

---
+++ p7/exec.js
@@ -0,0 +1,6 @@
+let vel = 40.5
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if (vel > 60) {
+    console.log('Você foi multado')
+}
+console.log('Dirija com cuidado!')
---
+++ p8/exec2.js
@@ -0,0 +1,7 @@
+let pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'BRA') {
+    console.log('Brasileiro')
+} else {
+    console.log('Estrangeiro')
+}
