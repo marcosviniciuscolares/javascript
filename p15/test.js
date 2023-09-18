dbe4d2c654509c39724088741fabbf4c8dd095fa
marcos vinicius <marcosviniciussilvacolares@gmail.com>
2023-09-18 09:20:28 -0300

    .

---
+++ p15/test.js
@@ -0,0 +1,11 @@
+let num = [2, 8 , 9, 7, 5]
+num.sort()
+num.push(1)
+
+console.log(num)
+console.log(`The array have ${num.length} positions`)
+console.log(`The first element is ${num[0]}`)
+
+let pos = num.indexOf(7)
+
+console.log(pos)
---
+++ p15/vetor.js
@@ -0,0 +1,14 @@
+/*
+for (let pos=0; pos < valores.length; pos++) {
+    console.log(`The position ${pos} have ${valores[pos]} value.`)
+}
+*/
+
+let valores = [8, 1, 7, 2, 1]
+
+console.log(valores)
+
+for (let pos in valores) {
+    console.log(`The psoition ${pos} have ${valores[pos]} value.`)
+}
+
