67c53f3885e286fdb55ad5c01cf1531151712b4c
marcos vinicius <marcosviniciussilvacolares@gmail.com>
2023-09-18 08:25:36 -0300

    .

---
+++ p14/ambiente.js
@@ -0,0 +1,12 @@
+/*let c = 1
+
+while (c<=10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+console.log('Vai começar...')
+for (let c = 1; c <= 5 ; c++) {
+    console.log(`Passo ${c}`)
+}
+console.log('FIM!')
