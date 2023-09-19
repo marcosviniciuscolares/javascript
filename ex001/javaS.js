ff39872da60c82e0fa011a21f3fa997a4265516f
marcos vinicius <marcosviniciussilvacolares@gmail.com>
2023-09-19 09:51:02 -0300

    .

---
+++ ex001/javaS.js
@@ -0,0 +1,8 @@
+function carregar() {
+    let msg = document.getElementById('msg')
+    let img = document.getElementById('imagem')
+    let data = new Date()
+    let hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} horas`
+}
