e7bc11cf6bb6e6c3295bdd5584492859f7fcf54a
marcos vinicius <marcosviniciussilvacolares@gmail.com>
2023-09-20 09:04:15 -0300

    .

--- ex001/javaS.js
+++ ex001/javaS.js
@@ -5,4 +5,12 @@ function carregar() {
     let hora = data.getHours()
 
     msg.innerHTML = `Agora são ${hora} horas`
+
+    if ( hora >= 0 && hora < 12 ) {
+        img.src = 'bomdia.jpg'
+    } else if ( hora >= 12 && hora < 18 ) {
+        img.src = 'boatarde.jpg'
+    } else {
+        img.src = 'boanoite.jpg'
+    }
 }
