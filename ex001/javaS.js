f5c46565e0f5fd74943a53828fe26bc88d55f213
marcos vinicius <marcosviniciussilvacolares@gmail.com>
2023-09-25 08:17:31 -0300

    .

--- ex001/javaS.js
+++ ex001/javaS.js
@@ -2,15 +2,18 @@ function carregar() {
     let msg = document.getElementById('msg')
     let img = document.getElementById('imagem')
     let data = new Date()
-    let hora = data.getHours()
+    let hora = 22
 
     msg.innerHTML = `Agora são ${hora} horas`
 
-    if ( hora >= 0 && hora < 12) {
-        img.src = 'manha.jpg'
-    } else if ( hora >= 12 && hora < 18) {
-        img.src = 'tarde.jpg'
+    if ( hora >= 0 && hora < 12 ) {
+        img.src = './img/manha.jpg'
+        document.body.style.background = '#FBF6A9'
+    } else if ( hora >= 12 && hora < 18 ) {
+        img.src = './img/tarde.jpg'
+        document.body.style.background = '#CB8354'
     } else {
-        img.src = 'noite.jpg'
+        img.src = './img/noite.jpg'
+        document.body.style.background = '#17384C'
     }
 }
