2ffcb97ec233a1d9f8bfdc4e67227c1ab2704b5d
marcos vinicius <marcosviniciussilvacolares@gmail.com>
2023-10-02 08:31:20 -0300

    .

--- ex002/javaS.js
+++ ex002/javaS.js
@@ -1,1 +1,20 @@
-alert('Olá')
+function verificar() {
+    let data = new Date()
+    let ano = data.getFullYear()
+    let fano = document.getElementById('iano')
+    let res = document.getElementById('res')
+
+    if (fano.value.length == 0 || fano.value > ano){
+        window.alert('ERRO, volte e coloque o ano do seu nascimento.')
+    } else {
+        let fsex = document.getElementsByName('radsex') 
+        let idade = ano - Number(fano.value)
+        let genero = ''
+
+        if (fsex[0].checked) {
+            genero = 'Homen'
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+        }
+    }
+}
