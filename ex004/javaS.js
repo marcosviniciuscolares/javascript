57bba97906ca65165bcb87d9ab9f01e7352f4139
marcos vinicius <marcosviniciussilvacolares@gmail.com>
2023-10-04 07:43:35 -0300

    .

--- ex004/javaS.js
+++ ex004/javaS.js
@@ -1,17 +1,17 @@
 function Gerar() {
     let numero = document.getElementById('inumero')
-    let atabu = document.getElementById('itabu')
+    let tabu = document.getElementById('itabu')
 
     if (numero.value.length == 0) {
         window.alert('[ERRO] Digite um número válido')
     } else {
         let n = Number(numero.value)
         let c = 1
-        tab.innerHTML = ''
+        tabu.innerHTML = ''
         while (c <= 10) {
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
-            tab.appendChild(item)
+            tabu.appendChild(item)
             c++
         }
     }
